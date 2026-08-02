a5a8ba18bb1b9b9ae12ba3fdd9ef4b17aa1f4c91
Dipjyoti Sarma <dipjyotisarma.ds2004@gmail.com>
2026-08-02 21:57:03 +0530

    add models

---
+++ main.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, Request, status
+from .models import Base
+from .database import engine
+from .routers import auth, todos, admin, users
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import RedirectResponse
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+
+Base.metadata.create_all(bind = engine)  #this will create database if it does not exist
+
+app.mount(
+    "/static",
+    StaticFiles(directory=str(BASE_DIR / "static")),
+    name="static"
+)
+
+@app.get("/")
+def test(request: Request):
+    return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)
+
+
+app.include_router(auth.router)
+app.include_router(todos.router)
+app.include_router(admin.router)
+app.include_router(users.router)
